c0ac7c2480efaa8764cae8c4e59b688e654934d3
SRIRAM VETURI <sriram.tutu@gmail.com>
2018-12-06 21:26:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,22 +19,21 @@ if __name__ == "__main__":
     # Read two train datasets
     df_comp_in = pd.read_csv('data/data-2_train.csv', sep='\s*,\s*')
     df_comp_out = pd.read_csv('data/Data-2_test.csv', sep='\s*,\s*')
+    
+    # Your output file name
     outFile = "output.txt"
 
     df_comp_out['class'] = np.ones(len(df_comp_out))
 
     df = pd.concat([df_comp_in, df_comp_out])
-    print('ok1')
     df = preprocessData(df)
-    print('ok2')
+    
     df, X, Y = aspectAnalysis(df)
-    print('ok3')
     X_train = X[0:len(df_comp_in)]
     Y_train = Y[0:len(df_comp_in)]
     X_test = X[len(df_comp_in):]
-    print('ok4')
+    
+    # Classifier
     classfier_comp = trainBestClassifier(X_train, Y_train)
-    print('ok5')
     Y_test = classfier_comp.predict(X_test)
-    print('ok6')
     printOutput(df_comp_out, Y_test, outFile)
